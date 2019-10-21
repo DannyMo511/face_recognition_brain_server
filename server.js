28fceafb6d3a258f7cf1487aac352419bbdbd950
Danny <DannyMo511@gmail.com>
2019-10-21 12:51:07 +0300

    removed unused get method

--- server.js
+++ server.js
@@ -27,11 +27,6 @@ const db = knex({
 
 const salt_rounds = 10;
 
-
-app.get('/', (req,res)=>{
-	res.json(database.users);
-});
-
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt, salt_rounds)});
 app.get("/profile/:id/", (req, res) => {profile.handleProfile(req, res, db)});
